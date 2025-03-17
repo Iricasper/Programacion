952f1e1dda33279a8c1148fd9e8b698e3ddb68c6
Iris <acasper631@g.educaand.es>
2025-03-17 13:12:15 +0100

    Terminado el ejercicio U5-Ej06

--- U5-Ejercicios/U5-Ej06/src/main/java/com/mycompany/u5/ej06/FiltrandoListas.java
+++ U5-Ejercicios/U5-Ej06/src/main/java/com/mycompany/u5/ej06/FiltrandoListas.java
@@ -27,7 +27,7 @@ public class FiltrandoListas {
         System.out.println(cadenas.toString());
         
         // Eliminamos las palabras acabadas en "o" y mostrar el contenido.
-        System.out.println("ELiminamos de la tabla las palabras acabadas en o");
+        System.out.println("\nEliminamos de la tabla las palabras acabadas en o.");
         Iterator iter = cadenas.iterator();
         
         while (iter.hasNext()) {
@@ -44,5 +44,45 @@ public class FiltrandoListas {
             System.out.println(cadena);
         }
         
+        // Eliminar palabras que contengan la letra "t". Mostrar la lista.
+        
+        iter = cadenas.iterator();
+        
+        while (iter.hasNext()) {
+            cadena = (String) iter.next();
+            if (cadena.contains("t")) {
+                iter.remove();
+            }
+        }
+        
+        System.out.println("\nEliminamos de la lista las palabras que contengan la t.");
+        iter = cadenas.iterator();
+        
+        while (iter.hasNext()) {
+            cadena = (String) iter.next();
+            System.out.println(cadena);
+        }
+        
+        // Eliminamos las palabras que contengan la letra "a" y que tengan más de 5 letras.
+        
+        System.out.println("\nEliminamos las que contengan la a y tengan +5 letras.");
+        
+        iter = cadenas.iterator();
+        
+        while (iter.hasNext()) {
+            cadena = (String) iter.next();
+            if (cadena.contains("a") && cadena.length() > 5) {
+                iter.remove();
+            }
+        }
+        
+        iter = cadenas.iterator();
+        
+        while (iter.hasNext()) {
+            cadena = (String) iter.next();
+            System.out.println(cadena);
+        }
+        
+        System.out.println("\nNo nos queda ninguna palabra en la lista.");
     }
 }
